a4e821b84c7cb1d492dbdf99693d3cee8dbddf65
NotAXaliks <vejmat276@gmail.com>
2025-10-29 11:40:46 +0300

    JWT начало

--- ChitChatApi/ChitChatApi/Controllers/EmployeeController.cs
+++ ChitChatApi/ChitChatApi/Controllers/EmployeeController.cs
@@ -1,8 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using ChitChatApi.Context;
 using ChitChatApi.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ChitChatApi.Controllers
 {
@@ -11,15 +16,17 @@ namespace ChitChatApi.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public EmployeeController(AppDbContext context)
+        public EmployeeController(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // api/employee/register
         [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] EmployeeDto dto)
+        public async Task<IActionResult> Register([FromBody] EmployeeRegisterDto dto)
         {
             /* Валидация */
             if (!ModelState.IsValid) return BadRequest(ModelState);
@@ -47,10 +54,65 @@ namespace ChitChatApi.Controllers
 
             return Ok(new { employee.Id, employee.Username });
         }
+
+        // api/employee/login
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] EmployeeLoginDto dto)
+        {
+            /* Валидация */
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            /* Конец валидации */
+
+            var employee = await _context.Employees.SingleAsync(e => e.Username == dto.Username);
+            if (employee == null) return BadRequest("Employee not found");
+
+            if (!BCrypt.Net.BCrypt.Verify(dto.Password, employee.Password))
+                return BadRequest("Invalid password");
+
+            var token = GenerateJwtToken(employee);
+
+            return Ok(new { token });
+        }
+
+        [Authorize]
+        [HttpGet("getMe")]
+        public async Task<IActionResult> GetMe()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null) return Unauthorized();
+
+            var employee = await _context.Employees.FindAsync(int.Parse(userId));
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+
+        private string GenerateJwtToken(Employee employee)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, employee.Id.ToString()),   // userId
+                // new Claim(JwtRegisteredClaimNames.UniqueName, employee.Username), // username
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims ?? [],
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:ExpiresInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
 
-public class EmployeeDto
+public class EmployeeRegisterDto
 {
     [Required(ErrorMessage = "Employee Name is required")]
     [MaxLength(100, ErrorMessage = "Max Employee Name length is 100")]
@@ -61,7 +123,17 @@ public class EmployeeDto
     [Required(ErrorMessage = "Employee Username is required")]
     [MaxLength(32, ErrorMessage = "Max Employee Username length is 32")]
     public required string Username { get; set; }
-    
+
+    [Required(ErrorMessage = "Employee Password is required")]
+    public required string Password { get; set; }
+}
+
+public class EmployeeLoginDto
+{
+    [Required(ErrorMessage = "Employee Username is required")]
+    [MaxLength(32, ErrorMessage = "Max Employee Username length is 32")]
+    public required string Username { get; set; }
+
     [Required(ErrorMessage = "Employee Password is required")]
     public required string Password { get; set; }
 }
--- ChitChatApi/ChitChatApi/Program.cs
+++ ChitChatApi/ChitChatApi/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using ChitChatApi.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,14 +10,30 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+        };
+    });
+builder.Services.AddAuthorization();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontendDev", policy =>
     {
-        policy.SetIsOriginAllowed(origin => true) 
+        policy.SetIsOriginAllowed(origin => true)
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials(); 
+            .AllowCredentials();
     });
 });
 
@@ -24,6 +43,8 @@ app.UseHttpsRedirection();
 app.UseCors("AllowFrontendDev");
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
